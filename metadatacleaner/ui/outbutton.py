509e4faee530840af9baa5e735d5a1ad1dbf5204
Romain Vigier <romain@romainvigier.fr>
2021-11-02 23:34:38 +0100

    Out button: Use GtkButton as base

--- metadatacleaner/ui/outbutton.py
+++ metadatacleaner/ui/outbutton.py
@@ -3,20 +3,26 @@
 
 """Fancy link button."""
 
-from gi.repository import GObject, Gtk
+from gi.repository import Gdk, GObject, Gtk
 
 
 @Gtk.Template(
     resource_path="/fr/romainvigier/MetadataCleaner/ui/OutButton.ui"
 )
-class OutButton(Gtk.LinkButton):
+class OutButton(Gtk.Button):
     """Fancy link button."""
 
     __gtype_name__ = "OutButton"
 
     icon_name = GObject.Property(type=str, nick="icon-name")
     label = GObject.Property(type=str)
+    uri = GObject.Property(type=str)
 
     def __init__(self, *args, **kwargs) -> None:
         """Button initialization."""
         super().__init__(*args, **kwargs)
+        self.set_cursor(Gdk.Cursor.new_from_name("pointer"))
+
+    @Gtk.Template.Callback()
+    def _on_clicked(self, button) -> None:
+        Gtk.show_uri(None, self.uri, Gdk.CURRENT_TIME)
